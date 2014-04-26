c3f678f49d3977b5e2dd45cd2975f40efee841cf
Jacob <jacobbev@gmail.com>
2014-04-25 23:18:32 -0400

    Started testing terrain generation.
    Bedrock now final.

---
+++ LD29/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainGenerator : MonoBehaviour {
+	public GameObject terrain;
+	// Use this for initialization
+	void Start () {
+		for(int x = -10 ; x < 10 ; x++) { //Testing please redo this for the love of god
+			for(int y = -5 ; y < 10 ; y++) {
+				Instantiate(terrain , new Vector2(x + 0.5f + (0.5f * terrain.transform.lossyScale.x) , y  + (0.5f * terrain.transform.lossyScale.y)) , gameObject.transform.rotation);
+				Instantiate(terrain , new Vector2(x + (0.5f * terrain.transform.lossyScale.x) , y  + 0.5f + (0.5f * terrain.transform.lossyScale.y)) , gameObject.transform.rotation);
+				Instantiate(terrain , new Vector2(x + 0.5f + (0.5f * terrain.transform.lossyScale.x) , y  + 0.5f + (0.5f * terrain.transform.lossyScale.y)) , gameObject.transform.rotation);
+				Instantiate(terrain , new Vector2(x + 0.5f * terrain.transform.lossyScale.x, y  + 0.5f * terrain.transform.lossyScale.y) , gameObject.transform.rotation);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
